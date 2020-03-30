c9313a850856bae0d87cb28a5f59373eb5c24211
Brendan Horst <yoshistar2000@gmail.com>
2020-03-30 16:16:37 -0400

    Added functionality to the filter
    Gave a POST request response to the '/' route that listens for the 'filter' form

--- flasktodo/todos.py
+++ flasktodo/todos.py
@@ -1,18 +1,42 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 from . import db
 
 
 bp = Blueprint("todos", __name__)
 
-@bp.route("/")
+@bp.route("/", methods=('GET', 'POST'))
 def index():
     """View for home page which shows list of to-do items."""
 
     cur = db.get_db().cursor()
+
+    if request.method == 'POST':
+        filter = request.form['filter']
+
+        if filter == 'all':
+
+            cur.execute('SELECT * FROM todos')
+            todos = cur.fetchall()
+            cur.close()
+            return render_template("index.html", todos=todos)
+
+        elif filter == 'completed':
+
+            cur.execute('SELECT * FROM todos WHERE completed=True')
+            todos = cur.fetchall()
+            cur.close()
+            return render_template("index.html", todos=todos)
+
+        elif filter == 'uncompleted':
+
+            cur.execute('SELECT * FROM todos WHERE completed=False')
+            todos = cur.fetchall()
+            cur.close()
+            return render_template("index.html", todos=todos)
+
     cur.execute('SELECT * FROM todos')
     todos = cur.fetchall()
     cur.close()
 
     return render_template("index.html", todos=todos)
-
