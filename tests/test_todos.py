426c731c3b16595a3f736ef45bc3d6ed5cfb42a0
Brendan Horst <yoshistar2000@gmail.com>
2020-03-31 13:53:39 -0400

    Wrote test for marking a task as completed

--- tests/test_todos.py
+++ tests/test_todos.py
@@ -6,6 +6,7 @@ def test_todo_list(client):
     assert b'<h1>A simple to-do application</h1>' in response.data
     assert b'clean room' in response.data
     assert b'<button type="submit">Apply' in response.data
+    assert b'<a href="complete/"' in response.data
 
     # Mock data should show three to-do items, one of which is complete
     assert response.data.count(b'<li class="">') == 2
@@ -21,3 +22,11 @@ def test_filter(client):
     response = client.post('/', data={'filter': 'uncompleted'})
     assert b'do homework' not in response.data
     assert b'clean room' in response.data
+
+def test_completion(client):
+
+    response = client.post('/', data={'filter': 'uncompleted'})
+    assert b'clean room' in response.data
+    response = client.get('/complete/1')
+    response = client.post('/', data={'filter': 'uncompleted'})
+    assert b'clean room' not in response.data
