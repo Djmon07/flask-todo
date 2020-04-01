0bed01851a41cde0f1af90d1e27cf7d0374c7af8
Brendan Horst <yoshistar2000@gmail.com>
2020-04-01 15:48:06 -0400

    Added a route that inputs newly registered users into the database

--- flasktodo/auth.py
+++ flasktodo/auth.py
@@ -3,3 +3,20 @@ from flask import (
 )
 
 from . import db
+
+bp = Blueprint("auth", __name__)
+
+
+@bp.route('/register', methods=('GET','POST'))
+def register():
+
+    if request.method == 'POST':
+
+        username = request.form['username']
+        password = request.form['password']
+
+        with db.get_db() as con:
+            with con.cursor() as cur:
+
+                cur.execute("""INSERT INTO users (username, password)
+                    VALUES (%s, %s)""", (username, password))
