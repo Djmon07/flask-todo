3163e1bc3e931eb62ad401b27f37f069de35f6d9
Daniel Monville <djmonwork@gmail.com>
2020-04-02 12:27:21 -0400

    added test for changing description

--- tests/test_todos.py
+++ tests/test_todos.py
@@ -23,11 +23,11 @@ def test_filter(client):
     assert b'do homework' not in response.data
     assert b'clean room' in response.data
 
-<<<<<<< HEAD
+
 def test_remove(client):
     response = client.post('/remove', data={'remove': 'do homework'})
     assert response.data.count(b'<li class="completed">') == 0
-=======
+
 def test_completion(client):
 
     response = client.post('/', data={'filter': 'uncompleted'})
@@ -35,4 +35,11 @@ def test_completion(client):
     response = client.get('/complete/1')
     response = client.post('/', data={'filter': 'uncompleted'})
     assert b'clean room' not in response.data
->>>>>>> ede6ebf0a4d01762a0c1597b65f30059b01ec3e7
+
+def test_description(client):
+
+    response = client.post('/' data={'filer':'uncomplete'})
+    assert b'get groceries' in response.data
+    reponse = client.get('/uncomplete/3')
+    reponse = glient.post('/', data={'filer': 'uncompleted'})
+    assert b'get groceries' not in response.data
